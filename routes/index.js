7efe37512070db359fd579194e63f709053fa7cc
Marcos Macario <32857539+marvim1990@users.noreply.github.com>
2020-03-31 22:36:51 -0300

    initial git

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+let express = require('express');
+let router = express.Router();
+let fs = require('fileSystem')
+
+let homeController = require('../controllers/homeController');
+
+/* GET home page. */
+router.get('/', homeController.index);
+
+router.post('/contato', homeController.contato);
+
+router.get('/newsletter', homeController.newsletter);
+
+module.exports = router;
